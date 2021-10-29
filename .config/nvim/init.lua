11cc06653e529cbbc47a532f39e156426f76049d
Icaro Ponce <ponceicaro@gmail.com>
2021-10-29 19:08:06 +0200

    updates to nvim + allacrity + sxhkd

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -27,7 +27,7 @@ opt.numberwidth = 2
 
 opt.backspace = {'indent', 'eol', 'start'}
 opt.pumheight = 20
-opt.completeopt = {'menuone', 'noselect'} -- Completion options
+opt.completeopt = {'menu','menuone', 'noselect'} -- Completion options
 -- opt.wildmode = {'list', 'longest'} -- Command-line completion mode
 
 opt.ignorecase = true -- ignore case when searching
@@ -66,8 +66,12 @@ opt.showmode = false -- get rido fo the -- INSERT --
 opt.clipboard = 'unnamedplus'
 cmd "set shortmess+=c"
 
-local packer = require('packer')
+g['mapleader'] = ','
 
+g['python3_host_prog'] = '~/.pyenv/versions/neovim3/bin/python'
+g['python_host_prog'] = '~/.pyenv/versions/neovim2/bin/python'
+
+local packer = require('packer')
 packer.startup(function()
   -- Packer can manage itself --
     use 'wbthomason/packer.nvim'
@@ -77,40 +81,93 @@ packer.startup(function()
     use 'tpope/vim-rhubarb'
     use 'sheerun/vim-polyglot'
 
-    use 'hrsh7th/nvim-compe'
     use 'nvim-treesitter/nvim-treesitter'
+    use {
+        "ray-x/lsp_signature.nvim",
+        config = 'require "lsp_signature".setup({ floating_window = true })',
+    }
     use 'neovim/nvim-lspconfig'
-    use 'junegunn/fzf.vim'
-    use {'iamcco/markdown-preview.nvim', run = 'cd app && yarn install', cmd = 'MarkdownPreview'}
+    use {
+        'hrsh7th/nvim-cmp',
+        requires = {
+            'hrsh7th/cmp-path',
+            'hrsh7th/cmp-calc',
+            'hrsh7th/cmp-emoji',
+            'hrsh7th/cmp-nvim-lsp',
+            'hrsh7th/cmp-buffer',
+            'L3MON4D3/LuaSnip',
+            'saadparwaiz1/cmp_luasnip',
+            'onsails/lspkind-nvim',
+        }
+    }
+
+    use {
+        'iamcco/markdown-preview.nvim',
+        run = 'cd app && yarn install',
+        cmd = 'MarkdownPreview'
+    }
+    use {
+        'nvim-telescope/telescope.nvim',
+        requires = { {'nvim-lua/plenary.nvim'} }
+    }
     use 'hoob3rt/lualine.nvim'
-    use 'ojroques/nvim-lspfuzzy'
-    use 'ryanoasis/vim-devicons'
     use 'machakann/vim-highlightedyank'
     use 'justinmk/vim-dirvish'
-    use 'windwp/nvim-autopairs'
 
-    use 'morhetz/gruvbox'
+    use {
+        'windwp/nvim-autopairs',
+        config = 'require "nvim-autopairs".setup()'
+    }
+
     use 'terryma/vim-expand-region'
+
     use 'lukas-reineke/indent-blankline.nvim'
 
-    use 'ap/vim-css-color' -- highlight hex and rgb colors
+    use 'EdenEast/nightfox.nvim'
+
+    use 'norcalli/nvim-colorizer.lua'
+
     use {'plasticboy/vim-markdown', requires = {'godlygeek/tabular'}}
 
     use 'tpope/vim-commentary'
-    use { 
+
+    use {
         'lewis6991/gitsigns.nvim',
-        requires = { 'nvim-lua/plenary.nvim' },
+        requires = {
+            'nvim-lua/plenary.nvim'
+        },
+        config = 'require "gitsigns".setup()'
+    }
+
+    use {
+        "kyazdani42/nvim-tree.lua",
+        requires = "kyazdani42/nvim-web-devicons",
     }
 end)
 
-cmd 'colorscheme gruvbox'
-g['gruvbox_italic'] = 1
-g['gruvbox_contrast_dark'] = 'medium'
+local colorizer = require 'colorizer'
+colorizer.setup {
+    '*';
+    css = { rgb_fn = true };
+    typescript = { rgb_fn = true };
+    javascript = { rgb_fn = true };
+}
+
+local nightfox = require 'nightfox'
+nightfox.setup({
+        fox = 'nightfox',
+        styles = {
+            comments = "italic", -- change style of comments to be italic
+            keywords = "bold", -- change style of keywords to be bold
+            functions = "italic" -- styles can be a comma separated list
+        },
+    })
+nightfox.load();
 
 local lualine = require 'lualine'
 lualine.setup({
-        options ={
-            theme = 'gruvbox',
+        options = {
+            theme = 'nightfox',
             icons_enabled = false,
             section_separators = '',
             component_separators = '',
@@ -118,60 +175,106 @@ lualine.setup({
         }
     })
 
-g['mapleader'] = ','
-
-g['python3_host_prog'] = '~/.pyenv/versions/neovim3/bin/python'
-g['python_host_prog'] = '~/.pyenv/versions/neovim2/bin/python'
-
--- gitsigns --
-local gitsigns = require 'gitsigns'
-gitsigns.setup {}
-
-
--- tree-sitter configuration --
 local ts = require 'nvim-treesitter.configs'
-ts.setup {ensure_installed = 'maintained', highlight = {enable = true}}
+ts.setup {
+    ensure_installed = 'maintained',
+    highlight = {enable = true}
+}
 
--- nvim-autopairs --
-local ap = require 'nvim-autopairs'
-ap.setup()
+-- telescope configuration --
+local telescope = require 'telescope'
+local telescope_actions = require "telescope.actions"
+telescope.setup {
+    defaults = {
+        mappings = {
+            i = {
+                ["<C-j>"] = telescope_actions.move_selection_next,
+                ["<C-k>"] = telescope_actions.move_selection_previous,
+            },
+        },
+    }
+}
+
+map('n', '<C-p>', '<cmd>Telescope find_files<cr>')
+map('n', '<leader>/', '<cmd>Telescope live_grep<cr>')
+map('n', "<leader>'", '<cmd>Telescope buffers<cr>')
+map('n', '<leader>fh', '<cmd>Telescope help_tags<cr>')
+
+-- nvim-tree configuration --
+cmd('autocmd BufEnter NvimTree set cursorline')
+-- cmd('highlight NvimTreeFolderIcon guibg=red')
+-- cmd('highlight NvimTreeOpenedFile guibg=red guifg=green')
+
+local tree = require 'nvim-tree'
+
+g.nvim_tree_indent_markers = 1
+g.nvim_tree_git_hl = 1
+g.nvim_tree_highlight_opened_files = 1
+
+tree.setup {
+    hijack_cursor = true;
+    update_focused_file = {
+        enable = true;
+    }
+}
+
+map('n', "<leader>t", '<cmd>NvimTreeToggle<CR>')
+map('n', '<leader>r', '<cmd>NvimTreeRefresh<CR>')
 
 -- lsp configuration --
 local lsp = require 'lspconfig'
-local lspfuzzy = require 'lspfuzzy'
-local compe = require 'compe'
+local cmp = require 'cmp'
+local lspkind = require "lspkind"
+local luasnip = require 'luasnip'
 
-compe.setup {
-    enabled = true;
-    autocomplete = true;
-    debug = true;
-    min_length = 1;
-    preselect = 'always';
-    throttle_time = 80;
-    source_timeout = 200;
-    resolve_timeout = 800;
-    incomplete_delay = 400;
-    max_abbr_width = 100;
-    max_kind_width = 100;
-    max_menu_width = 100;
-    documentation = true;
-
-    source = {
-        path = true;
-        buffer = true;
-        calc = true;
-        nvim_lsp = true;
-        nvim_lua = true;
-        ultisnips = true;
-    };
+cmp.setup {
+    -- compe.preselect = 'always' equivalent
+    -- completion = {
+    --    completeopt = 'menu,menuone,noinsert',
+    --  },
+    formatting = {
+        format = lspkind.cmp_format({with_text = true, maxwidth = 50})
+    },
+    snippet = {
+        expand = function(args)
+            require'luasnip'.lsp_expand(args.body)
+        end
+    },
+    mapping = {
+        ['<Tab>'] = function(fallback)
+            if cmp.visible() then
+                cmp.select_next_item()
+            elseif luasnip.expand_or_jumpable() then
+                vim.fn.feedkeys(vim.api.nvim_replace_termcodes('<Plug>luasnip-expand-or-jump', true, true, true), '')
+            else
+                fallback()
+            end
+        end,
+        ['<S-Tab>'] = function(fallback)
+            if cmp.visible() then
+                cmp.select_prev_item()
+            elseif luasnip.jumpable(-1) then
+                vim.fn.feedkeys(vim.api.nvim_replace_termcodes('<Plug>luasnip-jump-prev', true, true, true), '')
+            else
+                fallback()
+            end
+        end,
+        ['<C-Space>'] = cmp.mapping.complete(),
+        ['<C-e>'] = cmp.mapping.close(),
+        ['<CR>'] = cmp.mapping.confirm {
+            behavior = cmp.ConfirmBehavior.Replace,
+            select = true,
+        },
+    },
+    sources = {
+        { name = 'nvim_lsp' },
+        { name = 'luasnip' },
+        { name = 'calc' },
+        { name = 'emoji' },
+        { name = 'path' },
+        { name = 'buffer' },
+    }
 }
-
-
-cmd "inoremap <silent><expr> <CR>      compe#confirm('<CR>')"
-cmd "inoremap <silent><expr> <C-Space> compe#complete()"
--- cmd "inoremap <silent><expr> <C-e>     compe#close('<C-e>')"
--- cmd "inoremap <silent><expr> <C-f>     compe#scroll({ 'delta': +4 })"
--- cmd "inoremap <silent><expr> <C-d>     compe#scroll({ 'delta': -4 })"
 
 local format_async = function(err, result, ctx, config)
     if err ~= nil or result == nil then return end
@@ -238,10 +341,11 @@ end
 
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities.textDocument.completion.completionItem.snippetSupport = true
+capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
 
-lsp.pylsp.setup { on_attach=on_attach }
+lsp.pylsp.setup { on_attach = on_attach, capabilities = capabilities }
 lsp.tsserver.setup{
-    capabilities=capabilities,
+    capabilities = capabilities,
     filetypes = { 'javascript', 'typescript', 'typescriptreact' }, 
     root_dir = function() return vim.loop.cwd() end,
     on_attach = function(client)
@@ -249,10 +353,6 @@ lsp.tsserver.setup{
         on_attach(client)
     end
 }
-lspfuzzy.setup {
-    on_attach=on_attach,
-    root_dir = function() return vim.loop.cwd() end
-}  -- Make the LSP client use FZF instead of the quickfix list
 
 vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
         underline = true,
@@ -266,9 +366,6 @@ sign define LspDiagnosticsSignWarning text=🔸 linehl= numhl=
 sign define LspDiagnosticsSignInformation text=🔹 linehl= numhl=
 sign define LspDiagnosticsSignHint text=👉 linehl= numhl=
 ]]
-
-map('i', '<S-Tab>', 'pumvisible() ? "\\<C-p>" : "\\<Tab>"', {expr = true})
-map('i', '<Tab>', 'pumvisible() ? "\\<C-n>" : "\\<Tab>"', {expr = true})
 
 map('n', '<C-l>', '<cmd>noh<CR>')    -- Clear highlights
 map('n','<leader>h', ':Gitsigns preview_hunk<CR>')
@@ -312,7 +409,6 @@ local formatFiletypes = {
 lsp.diagnosticls.setup {
     on_attach = on_attach,
     filetypes = vim.tbl_keys(filetypes),
-    -- root_dir = function() return vim.loop.cwd() end,
     init_options = {
         filetypes = filetypes,
         linters = linters,
@@ -333,7 +429,3 @@ cmd 'cnoreabbrev WQ wq'
 cmd 'cnoreabbrev W w'
 cmd 'cnoreabbrev Q q'
 cmd 'cnoreabbrev Qall qall'
-
-g.fzf_layout = { window = { width=0.9, height=0.6}}
-cmd "noremap <leader>/ :Rg "
-cmd "nnoremap <C-p> :FZF<CR>"
